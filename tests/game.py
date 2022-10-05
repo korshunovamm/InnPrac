9e632b6fd9d6fc7d8ea4f04488f4e69e2f5ac449
chessnok <fadin.edu@gmail.com>
2022-10-05 16:31:43 +0300

    upd

---
+++ tests/game.py
@@ -0,0 +1,45 @@
+import unittest
+
+from game.game import Game
+
+
+class StaticBaseParams(unittest.TestCase):
+    game = Game()
+
+    def test_rooms_count(self):
+        self.assertEqual(60, self.game.rooms)
+
+    def test_staff_count(self):
+        self.assertEqual(120, self.game.staff['doctor'])
+        self.assertEqual(120, self.game.staff['lab_assistant'])
+
+    def test_equipment_count(self):
+        self.assertEqual(12, self.game.equipments['pre_analytic'])
+        self.assertEqual(12, self.game.equipments['reporting'])
+        self.assertEqual({
+            "yellow": 6,
+            "red": 6,
+            "blue": 6,
+            "green": 6,
+            "purple": 6,
+            "grey": 6
+        }, self.game.equipments['hand'])
+        self.assertEqual({
+            "yellow": 6,
+            "red": 6,
+            "blue": 6,
+            "green": 6,
+            "purple": 6,
+            "grey": 6
+        }, self.game.equipments['semi_manual'])
+        self.assertEqual({
+            "yellow": 6,
+            "red": 6,
+            "blue": 6,
+            "green": 6,
+            "purple": 6,
+            "grey": 6
+        }, self.game.equipments['auto'])
+
+    def max_pl_count(self):
+        self.assertEqual(6, self.game.max_pl_count)
